e0ceecab75bf7dc460163539cdd16e652cdcab53
umeboshi <umeboshi@9211740f-83e4-0310-b34f-f2d8bca2f096>
2004-09-28 01:02:31 +0000

    almost ready for a preview release

---
+++ trunk/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import os, sys
+from distutils.core import setup
+
+version = '0.1'
+description = 'Konsultant client management'
+author = 'Joseph Rawson'
+author_email = 'umeboshi@gregscomputerservice.com'
+url = 'http://developer.berlios.de/projects/konsultant'
+packages = ['konsultant']
+subpacks = ['base', 'db', 'managers', 'pdb', 'sqlgen']
+managers = ['client', 'ticket']
+
+packages += ['konsultant.%s' % p for p in subpacks]
+packages += ['konsultant.managers.%s' % p for p in managers]
+
+package_dir = {'' : 'lib'}
+scripts = ['konsultant']
+setup(version=version, description=description, author=author,
+      author_email=author_email, url=url, packages=packages,
+      package_dir=package_dir, scripts=scripts)
